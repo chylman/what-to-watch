91dba380004eb7a8884c48e45f7d33afc94cf4a5
Ilja <chylmanprosto@gmail.com>
2023-11-24 12:06:34 +0300

    fix: props error in main component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,19 +1,13 @@
 import Main from '../../pages/main/main';
 
 type AppProps = {
-    name: string
-    genre: string
-    releaseDate: string
-}
-
-const FILM_INFO = {
-  name: 'The Grand Budapest Hotel',
-  genre: 'Drama',
-  releaseDate: '2014'
+    name: string;
+    genre: string;
+    releaseDate: string;
 }
 function App(filmInfo: AppProps): JSX.Element {
   return (
-    <Main filmInfo={filmInfo}/>
+    <Main name={filmInfo.name} genre={filmInfo.genre} releaseDate={filmInfo.releaseDate}/>
   );
 }
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-const FILM_INFO = {
+const FILM_INFO : {
+  name: string;
+  genre: string;
+  releaseDate: string;
+} = {
   name: 'The Grand Budapest Hotel',
   genre: 'Drama',
   releaseDate: '2014'
-}
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,6 +18,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App filmInfo={FILM_INFO}/>
+    <App name={FILM_INFO.name} genre={FILM_INFO.genre} releaseDate={FILM_INFO.releaseDate}/>
   </React.StrictMode>,
 );
